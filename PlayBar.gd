1d9da7cdcbdb65131a1620c868f7159ac307ab3f
ccfortenberry <cmnky@hotmail.com>
2020-01-27 14:03:31 -0900

    Add project files

---
+++ PlayBar.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+export (float) var speed = 1.0
+
+var screen_size
+var velocity = Vector2(1,0)
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
